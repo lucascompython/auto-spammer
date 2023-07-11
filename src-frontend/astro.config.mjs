69b8a8078d98e14675bde4896f5635f275fcf044
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-07-11 10:41:31 +0100

    Only compress HTML if in PROD

--- src-frontend/astro.config.mjs
+++ src-frontend/astro.config.mjs
@@ -3,8 +3,11 @@ import compress from "astro-compress";
 
 import svelte from "@astrojs/svelte";
 
+import { loadEnv } from "vite";
+const { PROD } = loadEnv(import.meta.env.PROD, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  integrations: [compress(), svelte()]
+  compressHTML: PROD,
+  integrations: [compress(), svelte()],
 });
