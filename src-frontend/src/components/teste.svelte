ca9277e47f68dab2676f05dcefe3ffe80faad86d
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-07-12 23:44:41 +0100

    Update UI

--- src-frontend/src/components/teste.svelte
+++ src-frontend/src/components/teste.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { register } from "@tauri-apps/api/globalShortcut";
   import { invoke } from "@tauri-apps/api/tauri";
-  register("F6", async () => {
+  register("F6", () => {
     invoke("type_string", { string: "Hello World" });
   });
 </script>
