b4a6d7fd356682071344e2136d9a54bcec11527c
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-09-14 17:18:35 +0100

    Update deps and test autoclicker

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![no_main]
+use enigo::{KeyboardControllable, MouseControllable};
 
-use enigo::KeyboardControllable;
+static mut CLICKER: bool = false;
 
 #[tauri::command]
 fn type_string(string: String) {
@@ -10,10 +11,25 @@ fn type_string(string: String) {
     enigo.key_sequence_parse(&string);
 }
 
+#[tauri::command]
+fn autoclicker(speed: usize) {
+    unsafe {
+        CLICKER = !CLICKER;
+        if CLICKER {
+            std::thread::spawn(move || {
+                let mut enigo = enigo::Enigo::new();
+                loop {
+                    enigo.mouse_click(enigo::MouseButton::Left);
+                    std::thread::sleep(std::time::Duration::from_millis(speed as u64));
+                }
+            });
+        }
+    }
+}
 #[no_mangle]
 pub extern "C" fn main() -> usize {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![type_string])
+        .invoke_handler(tauri::generate_handler![type_string, autoclicker])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
     0
